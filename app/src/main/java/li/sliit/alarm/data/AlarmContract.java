3096ad5b1d8bfb964b51248f16e5dc5171d17b6d
rochana <rochana.atapattu2@gmail.com>
2019-05-04 15:32:49 +0530

    content provider

--- app/src/main/java/li/sliit/alarm/data/AlarmContract.java
+++ app/src/main/java/li/sliit/alarm/data/AlarmContract.java
@@ -1,9 +1,13 @@
 package li.sliit.alarm.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 public final class AlarmContract {
 
+    public final static String CONTENT_AUTHORITY  = "li.sliit.alarm";
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+    public static final String PATH_ALARM = "alarms";
     public AlarmContract() {
     }
 
@@ -15,5 +19,7 @@ public final class AlarmContract {
         public static final String COLUMN_NAME = "name";
         public static final String COLUMN_TIME = "time";
         public static final String COLUMN_ACTIVATED = "activated";
+
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI,PATH_ALARM);
     }
 }
---
+++ app/src/main/java/li/sliit/alarm/data/AlarmProvider.java
@@ -0,0 +1,7 @@
+package li.sliit.alarm.data;
+
+import android.content.ContentProvider;
+
+public class AlarmProvider {
+
+}
